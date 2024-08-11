60ea440b09b53e0ce31ca95cee06e3265ee08809
Bút <Cloly031128@gmail.com>
2024-08-11 21:39:16 +0700

    nested comment 1 level

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 import fastify from "fastify";
 import sensible from "@fastify/sensible";
+import cors from "@fastify/cors";
 import dotenv from "dotenv";
 import { PrismaClient } from "@prisma/client";
 dotenv.config();
 
 const app = fastify();
+app.register(sensible);
+app.register(cors, { origin: process.env.CLIENT_URL, credentials: true });
 const prisma = new PrismaClient();
 
 app.get("/posts", async (req, res) => {
@@ -12,6 +15,35 @@ app.get("/posts", async (req, res) => {
         select: {
             id: true,
             title: true,
+        },
+    });
+});
+
+app.get("/posts/:id", async (req, res) => {
+    return await prisma.post.findUnique({
+        where: {
+            id: req.params.id,
+        },
+        select: {
+            body: true,
+            title: true,
+            comments: {
+                orderBy: {
+                    createdAt: "desc",
+                },
+                select: {
+                    id: true,
+                    message: true,
+                    parentId: true,
+                    createdAt: true,
+                    user: {
+                        select: {
+                            id: true,
+                            name: true,
+                        },
+                    },
+                },
+            },
         },
     });
 });
